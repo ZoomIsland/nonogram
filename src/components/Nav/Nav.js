f56450571b067f7973de6bb4516aeac57af8e924
ZoomIsland <zacherymilan@gmail.com>
2020-12-28 15:39:29 -0700

    Added Nav, no links yet

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,3 +1,5 @@
+import { NavLink } from 'react-router-dom';
+
 import './Nav.css';
 
 function Nav() {
@@ -6,12 +8,19 @@ function Nav() {
       <div className="siteTitle">Nono Yaya</div>
       <div className="navLinks">
         <div className="allPuzzles">Puzzle Index</div>
-        <span>&nbsp;|&nbsp;</span>
+        <span className="lineDivide">&nbsp;|&nbsp;</span>
         <div className="randomPuzzle">Random Puzzle</div>
-        <span>&nbsp;|&nbsp;</span>
-        <div className="loginBtn">Login</div>
-        <span>&nbsp;|&nbsp;</span>
-        <div className="signupBtn">Sign up</div>
+        <span className="lineDivide">&nbsp;|&nbsp;</span>
+
+        {/* below is "logged in" version */}
+        <div className="newPuzzle">Create New</div>
+        <span className="lineDivide">&nbsp;|&nbsp;</span>
+        <div className="logoutBtn">Logout</div>
+
+        {/* below is the non-logged in version */}
+        {/* <div className="loginBtn">Login</div>
+        <span className="lineDivide">&nbsp;|&nbsp;</span>
+        <div className="signupBtn">Sign up</div> */}
       </div>
     </header>
   )
