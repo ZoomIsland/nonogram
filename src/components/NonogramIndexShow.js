d6ced71bdcdb0ab58b999afa130edb79b40bc6b3
ZoomIsland <zacherymilan@gmail.com>
2020-12-29 13:57:51 -0700

    Index now showing the solutions, which will be replaced by a question mark or other icon prior to finishing...eventually

--- src/components/NonogramIndexShow.js
+++ src/components/NonogramIndexShow.js
@@ -1,21 +1,51 @@
+import './IndexShow.css';
+
 function NonogramIndexShow(props) {
-  function createImage() {
-    // iterate through string
-    let count = 0;
-    for (let i = 0; i < props.data.nonogramString.length; i++) {
-      // run charAt i
-      // add element to final array?
-      //
+  function getImageStyle() {
+    const width = (props.data.width * 5) + "px";
+    const height = (props.data.height * 5) + "px";
+    return {
+      width: width,
+      height: height
     }
-    // make new row at width
-    // render small image based on that...
   }
+
+  function getBoxStyle(background) {
+    if (background === "X") {
+      background = "white";
+    }
+
+    return {
+      background: background,
+      width: "5px",
+      height: "5px",
+    }
+  }
+
+  // this is pretty close to how the creator renders...
+  // could I use a similar component?
+  const drawNonogram = props.data.nonogramArray.map((row, index) => {
+    return (
+    <div className="nonoRowSmall" key={index}>
+      {row.map((box, index) => {
+        return (
+          <div className="nonoBoxSmall" style={getBoxStyle(props.data.colorArray[box])}></div>
+        )
+      })}
+    </div>
+    )
+  })
 
   return (
     <div className="indexShowContainer">
-      <h2>{props.data.title}</h2>
-      <h3>By Author</h3>
-      <h4>Size: {props.data.width} x {props.data.height}</h4>
+      <div className="nonogramVisual" style={getImageStyle()}>
+        {drawNonogram}
+      </div>
+      <div className="indexDetails">
+        <h2>{props.data.title}</h2>
+        <h3>By Author</h3>
+        <h4>Size: {props.data.width} x {props.data.height}</h4>
+      </div>
     </div>
   )
 }
